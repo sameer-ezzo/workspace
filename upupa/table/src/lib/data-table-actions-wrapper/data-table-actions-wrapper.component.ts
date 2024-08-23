300bc3ea7014ee9129db3c59b270620225f7b277
Samir <ezzo.sameer@gmail.com>
2024-08-23 10:44:23 +0300

    chore: Refactor DataTableActionsWrapperComponent to read data from context

--- upupa/table/src/lib/data-table-actions-wrapper/data-table-actions-wrapper.component.ts
+++ upupa/table/src/lib/data-table-actions-wrapper/data-table-actions-wrapper.component.ts
@@ -1,39 +1,56 @@
-import { Component, EventEmitter, OnChanges, Input, Output, SimpleChanges, ChangeDetectionStrategy, signal, HostBinding } from '@angular/core'
-import { ActionDescriptor, ActionEvent } from '@upupa/common'
-
+import {
+  Component,
+  EventEmitter,
+  OnChanges,
+  Input,
+  Output,
+  SimpleChanges,
+  ChangeDetectionStrategy,
+  signal,
+  HostBinding,
+} from '@angular/core';
+import { ActionDescriptor, ActionEvent } from '@upupa/common';
 
 @Component({
-    selector: 'data-table-actions-wrapper',
-    templateUrl: './data-table-actions-wrapper.component.html',
-    styleUrls: ['./data-table-actions-wrapper.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'data-table-actions-wrapper',
+  templateUrl: './data-table-actions-wrapper.component.html',
+  styleUrls: ['./data-table-actions-wrapper.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DataTableActionsWrapperComponent<T = any> implements OnChanges {
-    @HostBinding('attr.tabindex') tabindex = 0
+  @HostBinding('attr.tabindex') tabindex = 0;
 
-    @Input() context: any
-    @Input() actions: ActionDescriptor[] | ((context: any) => ActionDescriptor[]) = []
-    // this represents the actions that will be shown in the header of the table
+  @Input() context: any;
+  @Input() actions:
+    | ActionDescriptor[]
+    | ((context: any) => ActionDescriptor[]) = [];
+  // this represents the actions that will be shown in the header of the table
 
-    @Output() action = new EventEmitter<ActionEvent>()
+  @Output() action = new EventEmitter<ActionEvent>();
 
-    _actions = signal<ActionDescriptor[]>([])
-    _menuActions = signal<ActionDescriptor[]>([])
+  _actions = signal<ActionDescriptor[]>([]);
+  _menuActions = signal<ActionDescriptor[]>([]);
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['actions'] || changes['context']) {
+      const { data } = this.context;
+      const items = data.map((x) => x.item);
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes['actions'] || changes['context']) {
-            const data = Array.isArray(this.context) ? this.context.map(x => x.item) : this.context?.item
+      const actions = Array.isArray(this.actions)
+        ? this.actions
+        : this.actions(items);
 
-            const actions = Array.isArray(this.actions) ? this.actions : this.actions(data)
-            this._actions.set(actions.filter(a => !a.menu))
-            this._menuActions.set(actions.filter(a => a.menu))
-        }
+      this._actions.set(actions.filter((a) => !a.menu));
+      this._menuActions.set(actions.filter((a) => a.menu));
     }
+  }
 
-    onAction(e: ActionEvent) {
-        const data = e.context.data
-        this.action.emit({ action: e.action, data: data.map(d => d.item as T), context: this.context })
-    }
+  onAction(e: ActionEvent) {
+    const data = e.context.data;
+    this.action.emit({
+      action: e.action,
+      data: data.map((d) => d.item as T),
+      context: this.context,
+    });
+  }
 }
-
