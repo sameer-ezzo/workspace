2fad3893bc8683734e7bf61a3344bedc87ae420b
Samir <ezzo.sameer@gmail.com>
2024-11-23 15:32:08 +0300

    refactor: simplify file browser component structure and update template and component logic

--- upupa/dynamic-form-native-theme/src/lib/upload/file-browser/file-browser.component.ts
+++ upupa/dynamic-form-native-theme/src/lib/upload/file-browser/file-browser.component.ts
@@ -1,21 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Input, forwardRef, OnInit, Injector, signal, inject, model, SimpleChanges, input } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from '@upupa/auth';
-import { EventBus } from '@upupa/common';
-import { DataAdapter, DataService, ApiDataSource } from '@upupa/data';
-import { LanguageService } from '@upupa/language';
-import { FileInfo } from '@upupa/upload';
-import { BehaviorSubject, Subscription } from 'rxjs';
-import { FileSelectComponent } from '../file-select/file-select.component';
-import { ValueDataComponentBase } from '@upupa/table';
-import { SnackBarService } from '@upupa/dialog';
+import { HttpClient } from "@angular/common/http";
+import { Component, Input, forwardRef, OnInit, Injector, signal, inject, model, SimpleChanges, input, computed } from "@angular/core";
+import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { AuthService } from "@upupa/auth";
+import { EventBus } from "@upupa/common";
+import { DataAdapter, DataService, ApiDataSource } from "@upupa/data";
+import { LanguageService } from "@upupa/language";
+import { FileInfo } from "@upupa/upload";
+import { BehaviorSubject, Subscription } from "rxjs";
+import { FileSelectComponent } from "../file-select/file-select.component";
+import { ValueDataComponentBase } from "@upupa/table";
+import { SnackBarService } from "@upupa/dialog";
+import { transform } from "lodash";
 
 @Component({
-    selector: 'file-browser',
-    templateUrl: './file-browser.component.html',
-    styleUrls: ['./file-browser.component.scss'],
+    selector: "file-browser",
+    templateUrl: "./file-browser.component.html",
+    styleUrls: ["./file-browser.component.scss"],
     providers: [
         {
             provide: NG_VALUE_ACCESSOR,
@@ -28,7 +29,6 @@ export class FileBrowserComponent extends ValueDataComponentBase<FileInfo> {
     private readonly data = inject(DataService);
     public auth = inject(AuthService);
     public http = inject(HttpClient);
-    public languageService = inject(LanguageService);
     public ds = inject(DataService);
     public route = inject(ActivatedRoute);
     public snack = inject(SnackBarService);
@@ -36,40 +36,45 @@ export class FileBrowserComponent extends ValueDataComponentBase<FileInfo> {
     normalizedChangeSub: Subscription | undefined;
 
     files = [];
-    view = signal<'list' | 'grid'>('list');
+    view = model<"list" | "grid">("list");
+
     focused = undefined as FileInfo | undefined;
 
-    keyProperty = '_id' as keyof FileInfo;
-    valueProperty = ['_id', 'fieldname', 'originalname', 'filename', 'size', 'encoding', 'mimetype', 'destination', 'path', 'date', 'status', 'user', 'meta'] as (keyof FileInfo)[];
+    keyProperty = "_id" as keyof FileInfo;
+    valueProperty = ["_id", "fieldname", "originalname", "filename", "size", "encoding", "mimetype", "destination", "path", "date", "status", "user", "meta"] as (keyof FileInfo)[];
 
-    path = input.required<string, string>({
+    path = input<string, string>("/", {
         transform: (v) =>
-            v
-                .replace(/\/$/, '')
-                .split('/')
+            (v ?? "")
+                .replace(/\/$/, "")
+                .split("/")
                 .filter((v) => v)
-                .join('/'),
+                .join("/"),
     });
-    override adapter = model(
-        new DataAdapter<FileInfo>(new ApiDataSource(this.data, '/storage', this.valueProperty), this.keyProperty, undefined, this.valueProperty, undefined, {
-            filter: {
-                destination: ['storage', this.path()].join('/'),
-            },
-            terms: [
-                { field: 'originalname' as keyof FileInfo, type: 'like' },
-                { field: 'fieldname' as keyof FileInfo, type: 'like' },
-            ],
-            page: { pageSize: 50 },
-        }),
-    );
+    segments = computed(() => this.path().split("/"));
+
+    override adapter = input<DataAdapter, DataAdapter>(null, {
+        transform: (v) =>
+            new DataAdapter<FileInfo>(new ApiDataSource(this.data, "/storage", this.valueProperty), this.keyProperty, undefined, this.valueProperty, undefined, {
+                filter: {
+                    destination: ["storage", this.path()].join("/"),
+                },
+                terms: [
+                    { field: "originalname" as keyof FileInfo, type: "like" },
+                    { field: "fieldname" as keyof FileInfo, type: "like" },
+                ],
+                page: { pageSize: 50 },
+            }),
+    });
+
     files$ = new BehaviorSubject<FileInfo[]>([]);
 
     override async ngOnChanges(changes: SimpleChanges): Promise<void> {
         super.ngOnChanges(changes);
-        if (changes['path']) {
+        if (changes["path"]) {
             this.normalizedChangeSub?.unsubscribe();
             const filter = {
-                destination: ['storage', this.path().split('/')].filter((v) => v).join('/'),
+                destination: ["storage", this.path().split("/")].filter((v) => v).join("/"),
             };
             this.adapter().filter = filter;
             this.normalizedChangeSub = this.adapter().normalized$.subscribe((f) => this.files$.next(f.map((c) => c.item)));
