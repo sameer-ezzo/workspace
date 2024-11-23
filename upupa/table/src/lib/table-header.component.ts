b4bef2d9e3da738200f6b0a6751847380068e23c
rami <ramyhhh@gmail.com>
2024-11-23 15:32:52 +0300

    upupa table make search box work again

--- upupa/table/src/lib/table-header.component.ts
+++ upupa/table/src/lib/table-header.component.ts
@@ -1,17 +1,21 @@
-import { Component, inject, input, model, Type } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { DynamicComponent, PortalComponent } from '@upupa/common';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, inject, input, model, Type } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { DynamicComponent, PortalComponent } from "@upupa/common";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { DataAdapter } from "@upupa/data";
+import { debounceTime, distinctUntilChanged, Subject, switchMap } from "rxjs";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
-    selector: 'table-header',
-    styleUrl: './table-header.component.scss',
+    selector: "table-header",
+    styleUrl: "./table-header.component.scss",
     standalone: true,
     imports: [PortalComponent, MatIconModule, FormsModule, MatButtonModule],
-    templateUrl: './table-header.component.html',
+    templateUrl: "./table-header.component.html",
 })
 export class TableHeaderComponent {
+    adapter = inject(DataAdapter);
     showSearch = input(true);
     // table = inject(DataTableComponent);
 
@@ -19,15 +23,19 @@ export class TableHeaderComponent {
         transform: (components) => components.map((c) => (c instanceof Type ? { component: c } : c)),
     });
 
-    q = model('');
+    q = model("");
 
-    // @Output() filterChange = new EventEmitter<FilterDescriptor>();
-    // onFilter(q: string) {
-    //     if (this.q() === q || (!this.q() && !q)) return;
-    //     this.table.loading.set(true);
-    //     this.q.set(q);
-    //     const f = { ...this.table.adapter.filter, ...(this.table.adapter.normalizeFilter(q) || {}) };
-    //     this.table.adapter.filter = f;
-    //     // this.filterChange.emit(f);
-    // }
+    filter$ = new Subject<string>();
+    constructor() {
+        this.filter$.pipe(takeUntilDestroyed(), debounceTime(500), distinctUntilChanged()).subscribe((q) => this._doFilter(q));
+    }
+    onFilter() {
+        this.filter$.next(this.q());
+    }
+
+    _doFilter(q) {
+        this.adapter.dataSource.terms = [{ field: "title", type: "like" }];
+        const f = { ...this.adapter.filter, ...(this.adapter.normalizeFilter(q) || {}) };
+        this.adapter.filter = f;
+    }
 }
