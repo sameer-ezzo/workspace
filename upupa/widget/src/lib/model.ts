e5108c62ab56768dc4e6cdd35acc78445d196964
rami <ramyhhh@gmail.com>
2024-12-23 00:10:39 +0300

    upupa widget library introduce

---
+++ upupa/widget/src/lib/model.ts
@@ -0,0 +1,46 @@
+import { component, DynamicComponent } from "@upupa/common";
+import type { GridStackWidget } from "gridstack";
+import { TextWidget } from "./widgets/text.widget";
+import { Class } from "@noah-ark/common";
+import { formInput, FormViewModelMirror } from "@upupa/dynamic-form";
+
+export type ComponentSelector = Omit<DynamicComponent, "component"> & { selector: string };
+export type Widget = GridStackWidget & {
+    id: string;
+    title: string;
+    icon?: string;
+    cssClass?: string;
+    template: ComponentSelector;
+};
+
+/**
+ * @description This model used to actually render the widget on the editor grid.
+ */
+export type MaterializedWidget<T = unknown> = Omit<Widget, "template"> & { template: DynamicComponent<T>; style?: Record<string, string> };
+
+/**
+ * @description
+ * A model used to build a widget object. Most properties are shared with the Widget model.
+ * id: will act as the selector for the widget (to render the real component)
+ * template: is the component representing the widget in the editor grid
+ * template.inputs: are the default inputs for the template component of the widget to be built.
+ */
+export type WidgetBlueprint<T = unknown> = Omit<Widget, "template"> & {
+    settingsForm: Class | FormViewModelMirror;
+    description?: string;
+    template: DynamicComponent<T>;
+};
+
+export function materializeWidget(baseWidget: Widget, blueprint: WidgetBlueprint): MaterializedWidget {
+    return {
+        ...baseWidget,
+        template: {
+            ...baseWidget.template,
+            component: blueprint.template.component,
+        },
+        style: {
+            "grid-column": `${baseWidget.x + 1} / span ${baseWidget.w ?? 1}`,
+            "grid-row": `${baseWidget.y + 1} / span ${baseWidget.h ?? 1}`,
+        },
+    };
+}
