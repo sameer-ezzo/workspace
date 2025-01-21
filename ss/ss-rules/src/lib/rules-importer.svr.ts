ed2d4303cf2da2d0b52265f2d0c4b53dba533949
Samir <ezzo.sameer@gmail.com>
2025-01-21 18:42:34 +0300

    refactor: remove commented logger statements from restore methods

--- ss/ss-rules/src/lib/rules-importer.svr.ts
+++ ss/ss-rules/src/lib/rules-importer.svr.ts
@@ -12,7 +12,7 @@ export class RulesImporterService {
     async restore(rules: RulePayload[], principle: Principle) {
         if (!rules || rules.length === 0) return;
         for (const r of rules) {
-            // logger.info(`restore`, r);
+            //
             await this.restoreRule(r, principle);
         }
         return { message: "Permissions restored successfully" };
@@ -21,21 +21,17 @@ export class RulesImporterService {
     actionsStr = (actions: Rule["actions"]) => Object.keys(actions).join(", ");
     async restoreRule(item: RulePayload, principle: Principle) {
         const rule = item.rule as Rule;
-        // logger.info(`restoreRule: ${rule.name}, ${this.actionsStr(rule.actions)}`);
+        //
         await this.restoreActions(rule.name, rule.actions);
         await this.restore(item.children, principle);
     }
 
     async restoreActions(name: string, actions: Rule["actions"], principle?: Principle) {
-        logger.info(`restoreActions: ${name}, ${this.actionsStr(actions)}`);
-
         if (!actions) return;
         const actionsKeys = Object.getOwnPropertyNames(actions);
         for (const action of actionsKeys) {
             const permissions = (actions[action] ?? []) as SimplePermission[];
             for (const permission of permissions) {
-                logger.info(`restoreAction: ${action}, ${permission._id}`);
-
                 if (!permission._id) continue; // prevent restoring permissions that are not saved in the database
                 try {
                     await this.rulesService.updatePermission(name, action, permission, principle);
