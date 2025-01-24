134ffae3bbc08c91fafd19cf3769dfa9fff71255
Samir <ezzo.sameer@gmail.com>
2025-01-24 18:55:43 +0300

    refactor: enhance API base URL handling in DataModule for better flexibility

--- upupa/data/src/lib/data.module.ts
+++ upupa/data/src/lib/data.module.ts
@@ -1,37 +1,36 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { APIBASE } from './di.token';
-import { DataConfig } from './model';
-
-
-
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { APIBASE } from "./di.token";
+import { DataConfig } from "./model";
+import { DOCUMENT } from "@angular/common";
 
 @NgModule({ imports: [], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class DataModule {
+    constructor() {
+        // if (parentModule) {
+        //   throw new Error('DataModule is already loaded. Import it in the AppModule only');
+        // }
+    }
 
-  constructor() {
-    // if (parentModule) {
-    //   throw new Error('DataModule is already loaded. Import it in the AppModule only');
-    // }
-  }
-
-
-  public static forChild(api_base: string, config?: DataConfig): ModuleWithProviders<DataModule> {
-
-    return {
-      ngModule: DataModule,
-      providers: [
-        {
-          provide: APIBASE,
-          useValue: api_base ?? '/api'
-        },
-        {
-          provide: DataConfig,
-          useValue: config
-        }
-      ]
-    };
-  }
-
-
+    public static forChild(api_base: string, config?: DataConfig): ModuleWithProviders<DataModule> {
+        return {
+            ngModule: DataModule,
+            providers: [
+                {
+                    provide: APIBASE,
+                    useFactory: (doc: Document) => {
+                        api_base = (api_base.length ? api_base : "/api").trim().toLocaleLowerCase();
+                        if (api_base.startsWith("http")) return api_base;
+                        const base = `${doc.location.protocol}//${doc.location.hostname}`;
+                        return new URL(api_base ?? "/api", base).toString();
+                    },
+                    deps: [DOCUMENT],
+                },
+                {
+                    provide: DataConfig,
+                    useValue: config,
+                },
+            ],
+        };
+    }
 }
