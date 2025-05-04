9aa1b192979f65f9fb7b886c7e956b778f6db229
rami <ramyhhh@gmail.com>
2025-05-04 23:43:00 +0300

    upupa-common(theme.service): refactor theme service to simplify initialization and apply logic

--- upupa/common/src/lib/theme.service.ts
+++ upupa/common/src/lib/theme.service.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT, isPlatformBrowser, isPlatformServer } from "@angular/common";
-import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
+import { inject, Inject, Injectable, PLATFORM_ID } from "@angular/core";
 
 export type Theme = {
     name: string;
@@ -12,45 +12,15 @@ export type Theme = {
     providedIn: "root",
 })
 export class ThemeService {
-    isBrowser!: boolean;
-    isServer!: boolean;
+    isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+
     themes: Theme[] = [];
     selectedTheme: Theme;
 
-    constructor(
-        @Inject(DOCUMENT) private document: Document,
-        @Inject(PLATFORM_ID) platformId: any,
-    ) {
-        this.isBrowser = isPlatformBrowser(platformId);
-        this.isServer = isPlatformServer(platformId);
-    }
+    constructor(@Inject(DOCUMENT) private document: Document) {}
 
-    init(themes: Theme[], autoApplySystemTheme: boolean = true) {
+    init(themes: Theme[]) {
         this.themes = themes;
-        const themesHasLight = themes.some((theme) => theme.colorScheme === "light");
-        const themesHasDark = themes.some((theme) => theme.colorScheme === "dark");
-
-        if (this.isBrowser && themesHasLight && themesHasDark) {
-            const mediaPrefersDark = window.matchMedia("(prefers-color-scheme: dark)");
-
-            if (autoApplySystemTheme) {
-                mediaPrefersDark.addEventListener("change", (e) => {
-                    const theme = this.findDefaultTheme();
-                    this.apply(theme.name);
-                });
-            }
-            const theme = this.findDefaultTheme();
-            this.apply(theme.name);
-        }
-    }
-
-    findDefaultTheme() {
-        const mediaPrefersDark = window.matchMedia("(prefers-color-scheme: dark)");
-        const currentPreference = mediaPrefersDark.matches ? "dark" : "light";
-        const lastTheme = localStorage.getItem("theme");
-        let currentTheme = this.themes.find((theme) => theme.name === lastTheme);
-        currentTheme ??= this.themes.find((theme) => theme.colorScheme === currentPreference) ?? this.themes[0];
-        return currentTheme;
     }
 
     apply(themeName: string) {
@@ -58,6 +28,9 @@ export class ThemeService {
         this.selectedTheme = theme;
         this.document.documentElement.classList.remove(...this.themes.flatMap((theme) => theme.className));
         this.document.documentElement.classList.add(...theme.className);
-        localStorage.setItem("theme", themeName);
+
+        if (this.isBrowser) {
+            localStorage.setItem("theme", themeName);
+        }
     }
 }
