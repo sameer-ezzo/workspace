de98653275c5a333bc51203ac3ee4db6df086f0a
Samir <ezzo.sameer@gmail.com>
2025-06-24 20:00:47 +0300

    feat: add range input base component and refactor date and numbers range components for reactive forms

--- upupa/dynamic-form-material-theme/src/lib/range-components/date-range/date-range.component.ts
+++ upupa/dynamic-form-material-theme/src/lib/range-components/date-range/date-range.component.ts
@@ -4,8 +4,10 @@ import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/fo
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
-import { MatNumbersRangeComponent } from "../numbers-range/numbers-range.component";
+import { RangeInputBaseComponent } from "../rang-input-base.component";
+import { InputDefaults } from "../../defaults";
 
+export type DateFilterFn = (date: Date | null) => boolean;
 @Component({
     selector: "mat-form-date-range-input",
     templateUrl: "./date-range.component.html",
@@ -19,6 +21,20 @@ import { MatNumbersRangeComponent } from "../numbers-range/numbers-range.compone
     ],
     imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatDatepickerModule],
 })
-export class MatDateRangeComponent extends MatNumbersRangeComponent {
+export class MatDateRangeComponent<T = Date> extends RangeInputBaseComponent<T> {
+    appearance = input(InputDefaults.appearance);
+    floatLabel = input(InputDefaults.floatLabel);
+    label = input("");
+    placeholder = input("");
+    hint = input("");
     readonly = input(false);
+
+    filterFn = input<DateFilterFn, DateFilterFn>(() => true, {
+        transform: (value) => {
+            if (typeof value === "function") {
+                return value;
+            }
+            return () => value === true;
+        },
+    });
 }
--- upupa/dynamic-form-material-theme/src/lib/range-components/numbers-range/numbers-range.component.ts
+++ upupa/dynamic-form-material-theme/src/lib/range-components/numbers-range/numbers-range.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from "@angular/common";
-import { Component, computed, forwardRef, input, model, SimpleChanges } from "@angular/core";
+import { Component, computed, forwardRef, input } from "@angular/core";
 import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
-import { InputBaseComponent } from "@upupa/common";
 import { InputDefaults } from "../../defaults";
+import { RangeInputBaseComponent } from "../rang-input-base.component";
 
 // https://angular-slider.github.io/ngx-slider/demos
 @Component({
@@ -14,7 +14,7 @@ import { InputDefaults } from "../../defaults";
     providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => MatNumbersRangeComponent), multi: true }],
     imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
 })
-export class MatNumbersRangeComponent<T = number> extends InputBaseComponent {
+export class MatNumbersRangeComponent<T = number> extends RangeInputBaseComponent<T> {
     appearance = input(InputDefaults.appearance);
     floatLabel = input(InputDefaults.floatLabel);
     label = input("");
@@ -35,9 +35,6 @@ export class MatNumbersRangeComponent<T = number> extends InputBaseComponent {
     thumbSize = input(75, {
         transform: (v: number) => Math.max(5, v),
     });
-
-    from = model<T | undefined>(undefined);
-    to = model<T | undefined>(undefined);
 
     _items = [];
     _options: {
@@ -61,61 +58,4 @@ export class MatNumbersRangeComponent<T = number> extends InputBaseComponent {
     items = computed(() => {
         return new Array(this.ceiling() - this.floor()).fill(0).map((_, i) => i);
     });
-
-    override async ngOnChanges(changes: SimpleChanges): Promise<void> {
-        await super.ngOnChanges(changes);
-        console.log("MatNumbersRangeComponent ngOnChanges", changes);
-        
-        if (changes["from"] || changes["to"]) {
-            this.value.set({
-                from: this.from(),
-                to: this.to(),
-            });
-        }
-        if (changes["control"] && this.control()) {
-            this.value.set(this.control().value);
-        }
-        if (changes["value"]) {
-            const value = this.value();
-            if (value) {
-                this.from.set(value.from);
-                this.to.set(value.to);
-            } else {
-                this.from.set(undefined);
-                this.to.set(undefined);
-            }
-        }
-    }
-
-    updateValue() {
-        this.handleUserInput({
-            from: this.from(),
-            to: this.to(),
-        });
-    }
-
-    // override propagateChange() {
-
-    //   // if (this.from > this.to || nullEmptyCheck(this.from) || nullEmptyCheck(this.to)) this._value = undefined;
-    //   // else this._value = { from: this.from, to: this.to }
-
-    //   this._value = { from: this.from, to: this.to }
-
-    //   const value = this._value;
-    //   if (this._onChange) this._onChange(value); //ngModel/ngControl notify (value accessor)
-    //   if (this.control) this.control.setValue(value); //control notify
-    //   this.valueChange.emit(value); //value event binding notify
-    //   this.value$.next(value); //template reference #component.value$ | async
-
-    // };
-
-    // override _updateViewModel() {
-    //     if (this._value) {
-    //         this.from = this._value.from;
-    //         this.to = this._value.to;
-    //     } else {
-    //         this.from = undefined;
-    //         this.to = undefined;
-    //     }
-    // }
 }
