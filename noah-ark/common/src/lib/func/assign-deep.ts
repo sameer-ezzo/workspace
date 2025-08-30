e0cf88a0b2374fda3a98ad596a48cd16808a74d1
Samir <ezzo.sameer@gmail.com>
2025-08-30 12:54:38 +0300

    feat: enhance deepAssign function to support multiple source objects

--- noah-ark/common/src/lib/func/assign-deep.ts
+++ noah-ark/common/src/lib/func/assign-deep.ts
@@ -1,9 +1,11 @@
-export function deepAssign(target: any, source: any): any {
-    for (const key in source) {
-        if (source[key] instanceof Object && key in target && target[key] instanceof Object) {
-            deepAssign(target[key] as any, source[key]);
-        } else {
-            target[key] = source[key];
+export function deepAssign(target: any, source: any, ...sources: any[]): any {
+    for (const s of [source, ...sources]) {
+        for (const key in s) {
+            if (s[key] instanceof Object && key in target && target[key] instanceof Object) {
+                deepAssign(target[key] as any, s[key]);
+            } else {
+                target[key] = s[key];
+            }
         }
     }
     return target;
