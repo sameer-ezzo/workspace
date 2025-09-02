394e4b2ddabf4972441150fabd4cad17992471b0
Samir <ezzo.sameer@gmail.com>
2025-09-03 00:12:28 +0300

    fix: use angular api to detect runtime platform

--- upupa/common/src/lib/event.bus.ts
+++ upupa/common/src/lib/event.bus.ts
@@ -1,23 +1,22 @@
-import { afterNextRender, Injectable } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { afterNextRender, inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { EventBusBase } from "@noah-ark/event-bus";
 
 @Injectable({ providedIn: "root" })
 export class EventBus extends EventBusBase {
+    private readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
     constructor() {
         super();
-        afterNextRender(() => {
-            this.init();
-        });
+        this.init();
     }
 
     async init() {
-        if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-            await navigator.serviceWorker.ready;
-            navigator.serviceWorker.onmessage = (event) => {
-                const data = event.data;
-                const topic = data.topic as string;
-                super.emit(topic.substring(14), data); //skip the 'notification:' prefix
-            };
-        }
+        if (this.isBrowser) return;
+        await navigator.serviceWorker.ready;
+        navigator.serviceWorker.onmessage = (event) => {
+            const data = event.data;
+            const topic = data.topic as string;
+            super.emit(topic.substring(14), data); //skip the 'notification:' prefix
+        };
     }
 }
